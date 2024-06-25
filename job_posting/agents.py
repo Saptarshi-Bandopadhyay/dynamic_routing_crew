cbbec79a0bc1a7fe7e1341766df09cbb27b2264e
Saptarshi Bandopadhyay <bandopadhyaysaptarshi@gmail.com>
2024-06-25 21:24:10 +0000

    fixing job generator

--- job_posting/agents.py
+++ job_posting/agents.py
@@ -1,10 +1,10 @@
 from crewai import Agent
-from crewai_tools.tools import WebsiteSearchTool, SeperDevTool, FileReadTool
+from crewai_tools.tools import WebsiteSearchTool, SerperDevTool, FileReadTool
 from langchain_community.llms import Ollama
 ollama_llama3 = Ollama(model="llama3")
 
-web_search_tool = WebsiteSearchTool()
-seper_dev_tool = SeperDevTool()
+# web_search_tool = WebsiteSearchTool()
+seper_dev_tool = SerperDevTool()
 file_read_tool = FileReadTool(
 	file_path='job_description_example.md',
 	description='A tool to read the job description example file.'
@@ -15,7 +15,8 @@ class Agents():
 		return Agent(
 			role='Research Analyst',
 			goal='Analyze the company website and provided description to extract insights on culture, values, and specific needs.',
-			tools=[web_search_tool, seper_dev_tool],
+			# tools=[web_search_tool, seper_dev_tool],
+			tools=[seper_dev_tool],
 			backstory='Expert in analyzing company cultures and identifying key values and needs from various sources, including websites and brief descriptions.',
 			verbose=True,
 			allow_delegation=False,
@@ -26,7 +27,8 @@ class Agents():
 			return Agent(
 				role='Job Description Writer',
 				goal='Use insights from the Research Analyst to create a detailed, engaging, and enticing job posting.',
-				tools=[web_search_tool, seper_dev_tool, file_read_tool],
+				tools=[seper_dev_tool, file_read_tool],
+				# tools=[web_search_tool, seper_dev_tool, file_read_tool],
 				backstory='Skilled in crafting compelling job descriptions that resonate with the company\'s values and attract the right candidates.',
 				verbose=True,
 				allow_delegation=False,
@@ -37,7 +39,8 @@ class Agents():
 			return Agent(
 				role='Review and Editing Specialist',
 				goal='Review the job posting for clarity, engagement, grammatical accuracy, and alignment with company values and refine it to ensure perfection.',
-				tools=[web_search_tool, seper_dev_tool, file_read_tool],
+				tools=[seper_dev_tool, file_read_tool],
+				# tools=[web_search_tool, seper_dev_tool, file_read_tool],
 				backstory='A meticulous editor with an eye for detail, ensuring every piece of content is clear, engaging, and grammatically perfect.',
 				verbose=True,
 				allow_delegation=False,
